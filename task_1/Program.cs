705d74bdde3bbe0946658fb7787d926cb4d8873a
unknown <excaliburbmw@mail.ru>
2022-06-06 20:12:44 +0200

    add task1

---
+++ task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int numberA = Convert.ToInt32(input);
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int numberB = Convert.ToInt32(input);
+
+int result = number * number;
+
+Console.WriteLine(number + " в квадрате будет " + result);

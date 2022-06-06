c3c7820204f2cd03eeb3d9c2c76a54693d221aad
unknown <excaliburbmw@mail.ru>
2022-06-06 20:45:07 +0200

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.Clear();
+
+Console.Write("Введите число:");
+
+string input = Console.ReadLine(); 
+int number = Convert.ToInt32(input);
+
+int min = -number;
+Console.WriteLine(min);
+
+
+while (min < number)
+{
+ min++;   
+ Console.WriteLine(min);
+}
